0bce7f93aaf96e761286b2b2e9ed859a9ac2425d
Karan Jaiswal <jaiskaran008@gmail.com>
2023-04-21 16:09:14 +0000

    optimised

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ form.addEventListener('submit', addItem);
 const todoList = document.querySelector('.todo-list');
 todoList.addEventListener('click',change);
 const todoCompletedList = document.querySelector('.todo-completed');
-const crudId="76c67933d02840cba82731d7c753da6b";
+const crudId="88ad028d5b14412798f151e88788651b";
 function addItem(e){
     e.preventDefault();
     const todo = {
@@ -12,44 +12,30 @@ function addItem(e){
         completed:false
     };
     axios.post('https://crudcrud.com/api/'+crudId+'/todoList',todo)
-    .then((res)=> addToList(todo))
+    .then((res)=> addToList(res.data))
     .catch((err)=>console.log(err));
     form.reset();
 }
 
 function addToList(todo){
     let newTodo = document.createElement('li');
-    newTodo.id=todo._id
-    
- 
-    newTodo.innerHTML=`<span>${todo.todoName} ${todo.desc}</span>
+    newTodo.id=todo._id;
+    if(!todo.completed){
+        newTodo.innerHTML=`<span>${todo.todoName} ${todo.desc}</span>
          <button class="checked btn m-1"> &#10004; </button>  <button class=" delete btn m-1">X</button>`
-    todoList.appendChild(newTodo);
-    
-    
+        todoList.appendChild(newTodo);
+    }
+    else{
+        newTodo.innerHTML=`<span>${todo.todoName} ${todo.desc}</span>`;
+        todoCompletedList.appendChild(newTodo);
+    }
 }
-function showCompletedTodos(todo){
-    let newTodo = document.createElement('li');
-    newTodo.id=todo._id
-    
-    newTodo.innerHTML=`<span>${todo.todoName} ${todo.desc}</span>`;
-        
-    todoCompletedList.appendChild(newTodo);
-}
+
 window.addEventListener('DOMContentLoaded',()=>{
     axios.get('https://crudcrud.com/api/'+crudId+'/todoList')
     .then((res )=> {
         for(let i=0;i<res.data.length;i++){
             addToList(res.data[i]);
-           
-        }
-    })
-    .catch((err)=>console.log(err));
-    axios.get('https://crudcrud.com/api/f0424399e70947e7875b02af8fc410e2/todo')
-    .then((res )=> {
-        for(let i=0;i<res.data.length;i++){
-            showCompletedTodos(res.data[i]);
-           
         }
     })
     .catch((err)=>console.log(err));
@@ -59,7 +45,6 @@ function change(e){
     var li = e.target.parentElement;
     var todos;
     let todo_id = li.id;
-    
     
     if(e.target.classList.contains('delete')){
         if(confirm('Are you sure to delete?')){
@@ -74,24 +59,16 @@ function change(e){
         axios.get('https://crudcrud.com/api/'+crudId+'/todoList/'+todo_id)
         .then((res)=>{
             
-            todos ={
+            todoUpdated ={
                 todoName:res.data.todoName,
                 desc: res.data.desc,
                 completed:true
             }
-            axios.post('https://crudcrud.com/api/f0424399e70947e7875b02af8fc410e2/todo',todos)
-            .then((res)=>{ 
-                axios.delete('https://crudcrud.com/api/'+crudId+'/todoList/'+todo_id)
-                .then(newRes => console.log(newRes.status))
-                .catch((err)=> console.log(err));
-                
-                showCompletedTodos(res.data);
-            
-            })
-        .catch((err)=>console.log(err));
+            axios.put('https://crudcrud.com/api/'+crudId+'/todoList/'+todo_id,todoUpdated)
+            .then(() => addToList(todoUpdated))
+            .catch((err)=>console.log(err));
         })
         .catch((err) => console.log(err));
         todoList.removeChild(li);
-        
     }
 }
